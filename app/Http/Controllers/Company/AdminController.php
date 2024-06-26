7ad6e44ac763bcb660b1f0830024a1d8e9a4ee0d
mishraarjun <arjun@xoniertechnologies.com>
2024-06-26 10:04:41 +0530

    updated code

--- app/Http/Controllers/Company/AdminController.php
+++ app/Http/Controllers/Company/AdminController.php
@@ -12,74 +12,65 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
-    private  $authService; 
+    private  $authService;
 
     public function __construct(AuthService $authService)
     {
         $this->authService = $authService;
-
     }
 
-/**
- * Process user login attempt.
- *      
- * This method handles the submission of the login form, validates the user input,
- * and attempts to authenticate the user using Laravel's built-in authentication system.
- *
- */
+    /**
+     * Process user login attempt.
+     *      
+     * This method handles the submission of the login form, validates the user input,
+     * and attempts to authenticate the user using Laravel's built-in authentication system.
+     *
+     */
     public function companyLogin(Request $request)
-    {    
+    {
 
-            try {
-                $validateUser = Validator::make($request->all(),[
-                    'email' => 'required|exists:company_users,email',
-                    'password' => 'required'
-                ]);
-                if ($validateUser->fails()) {
-                    return redirect('/signin')->withErrors($validateUser)->withInput();
-                }
-                $credentials = $request->only('email', 'password');
-                if (Auth::guard('admin')->attempt($credentials)) {
-                    return redirect('/dashboard');
-                }
-            } 
-            catch (\Throwable $th) {
-                return response()->json([
-                    'status' => false,
-                    'message' => $th->getMessage()
-                ], 500);
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'email' => 'required|exists:company_users,email',
+                'password' => 'required'
+            ]);
+            if ($validateUser->fails()) {
+                return redirect('/signin')->withErrors($validateUser)->withInput();
             }
+            $credentials = $request->only('email', 'password');
+            if (Auth::guard('admin')->attempt($credentials)) {
+                return redirect('/dashboard');
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+    /**
+     * Process user logout.
+     *
+     * This method logs out the authenticated user, clears the session data,
+     * and redirects the user to the login page.
+     */
+    public function companyLogout()
+    {
+        Auth::logout();
+        return redirect('signin');
+    }
+
+    public function signup()
+    {
+        return view('company.signup');
+    }
+    public function signin()
+    {
+        return view('company.signin');
+    }
+
+    public function super_admin_login_form()
+    {
+        return view('super_admin.account.login');
+    }
 }
-/**
- * Process user logout.
- *
- * This method logs out the authenticated user, clears the session data,
- * and redirects the user to the login page.
- */
-public function logout(Request $request)
-{
-    Auth::logout();
-    $request->session()->flash('success', 'You have been logged out.');
-
-    return redirect('signin');
-}
-
-public function signup()
-{        
-    return view('signup');
-}
-public function signin()
-{        
-    return view('signin');
-}
-
-public function super_admin_login_form()
-{        
-    return view('super_admin.account.login');
-}
-
-}
-
-
-
-
