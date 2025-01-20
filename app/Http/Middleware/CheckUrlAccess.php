b07fb3ff7e0ae07dd0cd5b3ff5f12d2aa62ba03f
mishraarjun <arjun@xoniertechnologies.com>
2025-01-20 10:29:06 +0530

    menu access in company panel updated code

---
+++ app/Http/Middleware/CheckUrlAccess.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\Menu;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+use Spatie\Permission\Exceptions\UnauthorizedException;
+
+class CheckUrlAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next): Response
+    {
+        if (Auth::guard('company')->check()) {
+            $currentUrl = '/' . $request->path();
+            if ($currentUrl != '/company/dashboard') {
+                $accessReponse = $this->checkMenuDetails($currentUrl, Auth::guard('company')->user()->company_id);
+                if ($accessReponse)
+                    return $next($request);
+                else
+                    throw UnauthorizedException::notAcessRoute();
+            }
+        }
+        if (Auth::guard('employee')->check()) {
+            $currentUrl = '/' . str_replace('employee', 'company', $request->path());
+            if ($currentUrl != '/employee/dashboard') {
+                $accessReponse = $this->checkMenuDetails($currentUrl, Auth::guard('employee')->user()->company_id);
+                if ($accessReponse)
+                    return $next($request);
+                else
+                    throw UnauthorizedException::notAcessRoute();
+            }
+        }
+        return $next($request);
+    }
+
+    private function checkMenuDetails($currentUrl, $companyId)
+    {
+        return Menu::whereHas('company', function ($query) use ($companyId) {
+            $query->where('id', $companyId);
+        })->where('slug', $currentUrl)->exists();
+    }
+}
