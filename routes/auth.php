8ee79a6fd49e130a53149550122b7c24545e1ef5
nancy-xonier <nancy@xoniertechnologies.com>
2025-01-28 14:58:24 +0530

    use guest guard for guest routes

--- routes/auth.php
+++ routes/auth.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\EmployeeComplainController;
 use Illuminate\Support\Facades\Artisan;
 
 /**---------------Reset And Forget Password Route----------------*/
-Route::controller(ForgetPasswordController::class)->group(function () {
+Route::middleware('guest')->controller(ForgetPasswordController::class)->group(function () {
     Route::get('/forget/password', 'index')->name('forget.password');
     Route::post('/submit/ForgetPassword/Form', 'submitForgetPasswordForm')->name('submitForgetPasswordForm');
     Route::get('reset-password/{token}', 'showResetPasswordForm')->name('reset.password.get');
@@ -20,7 +20,7 @@ Route::controller(ForgetPasswordController::class)->group(function () {
 /**---------------End Reset And Forget Password Route----------------*/
 
 /**---------------Reset And Forget Password Route FOR Company----------------*/
-Route::prefix('company')->controller(CompanyForgetPasswordController::class)->group(function () {
+Route::prefix('company')->middleware('guest')->controller(CompanyForgetPasswordController::class)->group(function () {
     Route::get('/forget/password', 'index')->name('company.forget.password');
     Route::post('/submit/ForgetPassword/Form', 'submitForgetPasswordForm')->name('company.submitForgetPasswordForm');
     Route::get('reset-password/{token}', 'showResetPasswordForm')->name('company.reset.password.get');
@@ -29,7 +29,7 @@ Route::prefix('company')->controller(CompanyForgetPasswordController::class)->gr
 /**---------------End Reset And Forget Password Route FOR Company----------------*/
 
 /**---------------Reset And Forget Password Route FOR Admin----------------*/
-Route::prefix('admin')->controller(AdminForgetPasswordController::class)->group(function () {
+Route::prefix('admin')->middleware('guest')->controller(AdminForgetPasswordController::class)->group(function () {
     Route::get('/forget/password', 'index')->name('admin.forget.password');
     Route::post('/submit/ForgetPassword/Form', 'submitForgetPasswordForm')->name('admin.submitForgetPasswordForm');
     Route::get('reset-password/{token}', 'showResetPasswordForm')->name('admin.reset.password.get');
@@ -42,35 +42,41 @@ Route::prefix('admin')->controller(AdminForgetPasswordController::class)->group(
 Route::get('/', [AuthController::class, 'index'])->name('employee');
 
 Route::prefix('employee')->controller(AuthController::class)->group(function () {
-    Route::post('/login', 'employeeLogin')->name('employee.login');
+    Route::middleware('guest')->group(function () {
+        Route::post('/login', 'employeeLogin')->name('employee.login');
+        Route::get('/verify/otp', 'verifyOtp')->name('employee.verifyOtp');
+        Route::post('/verify/otp/submit', 'verifyOtpCheck')->name('employee.verifyOtpCheck');
+        Route::get('/resend/otp', 'resendOtp')->name('employee.resendOtp');
+    });
     Route::get('/logout', 'emoloyeeLogout')->name('employee.logout');
-    Route::get('/verify/otp', 'verifyOtp')->name('employee.verifyOtp');
-    Route::post('/verify/otp/submit', 'verifyOtpCheck')->name('employee.verifyOtpCheck');
-    Route::get('/resend/otp', 'resendOtp')->name('employee.resendOtp');
     Route::post('/change/password','changePassword')->name('change.password');
 });
 /**---------------End Employee Auth Route----------------*/
 
 /**---------------Company Panel Route----------------*/
 Route::prefix('company')->controller(AdminController::class)->group(function () {
-    Route::post('/login', 'companyLogin')->name('company.login');
     Route::get('/logout', 'companyLogout')->name('company.logout');
-    Route::get('/signin', 'signin')->name('signin');
+    Route::middleware('guest')->group(function () {
+        Route::post('/login', 'companyLogin')->name('company.login');
+        Route::get('/signin', 'signin')->name('signin');
+        Route::get('/signup', 'signup')->name('signup');
+        Route::get('/resend/otp', 'resendOtp')->name('company.resendOtp');
+        Route::get('/verify/otp', 'verifyOtp')->name('verifyOtp');
+        Route::post('/verify/otp/submit', 'verifyOtpCheck')->name('verifyOtpCheck');
+    });
     // Route::get('/forget/password', 'forgetPassword')->name('signin');
-    Route::get('/signup', 'signup')->name('signup');
-    Route::get('/resend/otp', 'resendOtp')->name('company.resendOtp');
-    Route::get('/verify/otp', 'verifyOtp')->name('verifyOtp');
-    Route::post('/verify/otp/submit', 'verifyOtpCheck')->name('verifyOtpCheck');
 });
 /**---------------End Company Auth Route----------------*/
 
 /** ----------------- Super Admin Started -------------------- **/
 Route::prefix('/admin')->controller(AdminAuthController::class)->group(function () {
-    Route::get('/login', 'login')->name('admin.login.form');
-    Route::get('/verify/otp', 'verifyOtp')->name('admin.verifyOtp');
-    Route::get('/resend/otp', 'resendOtp')->name('admin.resendOtp');
-    Route::post('/verify/otp/submit', 'verifyOtpCheck')->name('admin.verifyOtpCheck');
-    Route::post('/admin_login', 'admin_login')->name('super.admin.login');
+    Route::middleware('guest')->group(function () {
+        Route::get('/login', 'login')->name('admin.login.form');
+        Route::get('/verify/otp', 'verifyOtp')->name('admin.verifyOtp');
+        Route::get('/resend/otp', 'resendOtp')->name('admin.resendOtp');
+        Route::post('/verify/otp/submit', 'verifyOtpCheck')->name('admin.verifyOtpCheck');
+        Route::post('/admin_login', 'admin_login')->name('super.admin.login');
+    });
     Route::get('/logout', 'adminLogout')->name('admin.logout');
 });
 /**---------------End Super Admin Auth Route----------------*/
