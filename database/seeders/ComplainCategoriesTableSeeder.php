be9b4ffcf7533c41245d1994243030ca0773a686
Siddharth Pandey <sid@xoniertechnologies.com>
2025-01-30 13:05:50 +0530

    fix: complain related models, migrations and seeders

--- database/seeders/ComplainCategoriesTableSeeder.php
+++ database/seeders/ComplainCategoriesTableSeeder.php
@@ -14,63 +14,68 @@ class ComplainCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('complain_categories')->delete();
-        
-        \DB::table('complain_categories')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'name' => 'HR',
-                'description' => NULL,
-                'status' => 1,
-                'company_id' => 1,
-                'created_at' => '2024-07-15 13:03:51',
-                'updated_at' => '2024-07-15 13:03:51',
-            ),
-            1 => 
-            array (
-                'id' => 2,
-                'name' => 'IT',
-                'description' => 'IT related',
-                'status' => 1,
-                'company_id' => 1,
-                'created_at' => '2024-07-15 13:04:04',
-                'updated_at' => '2024-07-15 13:04:04',
-            ),
-            2 => 
-            array (
-                'id' => 3,
-                'name' => 'Attendance',
-                'description' => NULL,
-                'status' => 1,
-                'company_id' => 1,
-                'created_at' => '2024-07-15 13:04:24',
-                'updated_at' => '2024-07-15 13:04:24',
-            ),
-            3 => 
-            array (
-                'id' => 4,
-                'name' => 'Productive complaining',
-                'description' => NULL,
-                'status' => 1,
-                'company_id' => 1,
-                'created_at' => '2024-07-15 13:05:19',
-                'updated_at' => '2024-07-15 13:05:19',
-            ),
-            4 => 
-            array (
-                'id' => 5,
-                'name' => 'Venting',
-                'description' => NULL,
-                'status' => 1,
-                'company_id' => 1,
-                'created_at' => '2024-07-15 13:05:30',
-                'updated_at' => '2024-07-15 13:05:30',
-            ),
+
+        \DB::table('complain_categories')->insert(array(
+            0 =>
+                array(
+                    'id' => 1,
+                    'name' => 'HR',
+                    'description' => NULL,
+                    'status' => 1,
+                    'company_id' => 1,
+                    'created_by' => 1,
+                    'created_at' => '2024-07-15 13:03:51',
+                    'updated_at' => '2024-07-15 13:03:51',
+                ),
+            1 =>
+                array(
+                    'id' => 2,
+                    'name' => 'IT',
+                    'description' => 'IT related',
+                    'status' => 1,
+                    'company_id' => 1,
+                    'created_by' => 1,
+                    'created_at' => '2024-07-15 13:04:04',
+                    'updated_at' => '2024-07-15 13:04:04',
+                ),
+            2 =>
+                array(
+                    'id' => 3,
+                    'name' => 'Attendance',
+                    'description' => NULL,
+                    'status' => 1,
+                    'company_id' => 1,
+                    'created_by' => 1,
+                    'created_at' => '2024-07-15 13:04:24',
+                    'updated_at' => '2024-07-15 13:04:24',
+                ),
+            3 =>
+                array(
+                    'id' => 4,
+                    'name' => 'Productive complaining',
+                    'description' => NULL,
+                    'status' => 1,
+                    'company_id' => 1,
+                    'created_by' => 1,
+                    'created_at' => '2024-07-15 13:05:19',
+                    'updated_at' => '2024-07-15 13:05:19',
+                ),
+            4 =>
+                array(
+                    'id' => 5,
+                    'name' => 'Venting',
+                    'description' => NULL,
+                    'status' => 1,
+                    'company_id' => 1,
+                    'created_by' => 1,
+                    'created_at' => '2024-07-15 13:05:30',
+                    'updated_at' => '2024-07-15 13:05:30',
+                ),
         ));
-        
-        
+
+
     }
 }
