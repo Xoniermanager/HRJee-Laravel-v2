3cf3edfc39af07e5dafc08bf4c4a52e695a7b963
nancy-xonier <nancy@xoniertechnologies.com>
2025-02-07 15:28:59 +0530

    Worked on APIs for user profile update and attendance for particular date

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,15 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SendOtpRequest;
 use App\Http\Requests\UserLoginRequest;
-use App\Http\Requests\UserPasswordChangeRequest;
 use App\Http\Requests\UserRequest;
-use App\Http\Requests\UserUpdateProfileRequest;
 use App\Http\Services\Api\AuthService;
 use App\Models\User;
 use App\Models\MenuRole;
 use App\Models\Menu;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -85,9 +82,35 @@ class AuthController extends Controller
             'data' => [],
         ], 200);
     }
-    public function updateProfile(UserUpdateProfileRequest $request)
+    public function updateProfile(Request $request)
     {
-        return $this->userAuthService->updateProfile($request);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'father_name' => 'required|string|max:100',
+            'mother_name' => 'required|string|max:100',
+            'blood_group' => 'required|in:A+, A-, B+, B-, O-, O+',
+            'gender' => 'required|in:M,F,O',
+            'marital_status' => 'required|in:M,S',
+            'phone' => 'required|min:5|max:20',
+            'profile_image' => 'nullable|mimes:jpeg,png,jpg',
+            'date_of_birth' => 'required|date|before:' . now()->subYears(18)->toDateString(),
+        ]);
+
+        if ($validator->fails()) {
+
+            return response()->json([
+                "error" => 'validation_error',
+                "message" => $validator->errors(),
+            ], 400);
+        }
+
+        $this->userAuthService->updateProfile($request);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Profile updated successfully',
+            'data' => [],
+        ], 200);
     }
 
     public function getCompanyDetails()
