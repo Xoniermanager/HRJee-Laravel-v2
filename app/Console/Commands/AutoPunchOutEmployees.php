a773d9f70e18fa0cfd5199884a2d6db99abbbd85
Siddharth Pandey <sid@xoniertechnologies.com>
2025-06-30 15:41:37 +0530

    fix: attendance out issue also in auto auto punch out job
    issue: wrong calculation for half day, short leave and late

--- app/Console/Commands/AutoPunchOutEmployees.php
+++ app/Console/Commands/AutoPunchOutEmployees.php
@@ -15,15 +15,28 @@ class AutoPunchOutEmployees extends Command
         $now = Carbon::now();
         // Get all records where punch out is null
         $attendances = EmployeeAttendance::whereNull('punch_out')->get();
+        // dd($attendances->where('user_id', 10));
         foreach ($attendances as $attendance) {
-            $autoPunchoutTime = (int) $attendance->shift->auto_punch_out; // in minutes
-            $shiftEndTime = $attendance->shift->end_time;
+            $officeShiftDetails = $attendance->shift;
+            $autoPunchoutTime = (int) $officeShiftDetails->auto_punch_out;
+            $shiftEndTime = $officeShiftDetails->end_time;
             $shiftEnd = Carbon::parse($shiftEndTime);
             $forceOutTime = $shiftEnd->copy()->addMinutes($autoPunchoutTime);
+
             // Perform auto punch out if current time >= force out time and not already punched out
             if (now()->gte($forceOutTime) && !$attendance->punch_out) {
+                $now = Carbon::now();
+                [, , $attendanceStatus] = checkForHalfDayAttendance(
+                    $officeShiftDetails->toArray(),
+                    $officeShiftDetails->officeTimingConfigs->toArray(),
+                    $now->format('Y/m/d'),
+                    Carbon::parse($attendance->punch_in),
+                    $now
+                );
+
                 $attendance->punch_out = $forceOutTime;
                 $attendance->is_auto_punch_out = 1;
+                $data['status'] = $attendanceStatus;
                 $attendance->save();
                 $this->info("Auto punched out user {$attendance->user_id} at {$forceOutTime}");
             }
